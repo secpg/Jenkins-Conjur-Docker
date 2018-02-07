76b3a93b7cb6ca292a36acb5ff86430b3c242415
Pete Greenwood <ptr.grnwd@gmail.com>
2018-02-06 21:30:23 -0500

    Update jenkins_start.sh

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-docker volume create jenkins
-
-docker network create jenkins
 
 docker container run -d --name jenkins-master -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home --restart=always secpg/demo_conjur_docker_jenkins
